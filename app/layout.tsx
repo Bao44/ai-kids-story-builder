23d1d097c08f88af27cb5af0de43eef19d49ba94
Bao44 <tqbao44@gmail.com>
2024-12-25 10:46:24 +0700

    set font and created StorySubjectInput

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
+import {Nunito} from 'next/font/google'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const MyAppFont = Nunito({subsets:['latin']});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={MyAppFont.className}
       >
         <Provider>{children}</Provider>
       </body>
--- app/provider.tsx
+++ app/provider.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { NextUIProvider } from "@nextui-org/react";
 import { ClerkProvider } from "@clerk/nextjs";
+import Header from "./_components/Header";
 
 function Provider({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <ClerkProvider>
-        <NextUIProvider>{children}</NextUIProvider>
+        <NextUIProvider>
+          {/* Header */}
+          <Header />
+          {children}
+        </NextUIProvider>
       </ClerkProvider>
     </div>
   );
