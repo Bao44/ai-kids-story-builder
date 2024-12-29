b9a8ec128d3a0bb2966baf6fa3596b02504eafe1
Bao44 <tqbao44@gmail.com>
2024-12-29 19:49:03 +0700

    implement dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import DashboardHeader from './_components/DashboardHeader'
 
 function Page() {
   return (
-    <div>
-      
+    <div className='p-10 md:px-20 lg:px-40 min-h-screen'>
+      <DashboardHeader />
     </div>
   )
 }
