65aee4e2ca73e02755624d75d2447bff1e3e8195
Bao44 <tqbao44@gmail.com>
2024-12-30 11:18:33 +0700

    completed dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import DashboardHeader from './_components/DashboardHeader'
+import UserStoryList from './_components/UserStoryList'
 
 function Page() {
   return (
     <div className='p-10 md:px-20 lg:px-40 min-h-screen'>
       <DashboardHeader />
+      <UserStoryList />
     </div>
   )
 }
